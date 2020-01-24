00f6d838911d391dc1ea9dfa08282c00bd58bc70
Rupeng Liu <rupliu@microsoft.com>
2020-01-23 18:41:11 -0800

    Import function app

--- src/utils/requestUtil.ts
+++ src/utils/requestUtil.ts
@@ -4,16 +4,37 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { WebResource } from "ms-rest";
+import { ServiceClientCredentials } from "ms-rest";
 import * as request from 'request-promise';
 import { appendExtensionUserAgent } from "vscode-azureextensionui";
+import { IFunctionHostKeyResponse } from "../models/Function";
+import { signRequest } from "./signRequest";
 
-export async function requestUtil(url: string) : Promise<string> {
+export type nRequest = WebResource & request.RequestPromiseOptions;
+
+export async function requestUtil(url: string): Promise<string> {
     const requestOptions: WebResource = new WebResource();
     requestOptions.headers = {
         ['User-Agent']: appendExtensionUserAgent()
     };
     requestOptions.url = url;
-// tslint:disable-next-line: await-promise
+    // tslint:disable-next-line: await-promise
     const response = await request(requestOptions).promise();
-    return  <string>(response);
+    return <string>(response);
+}
+
+export async function requestUtilWithCredentials(url: string, credentials: ServiceClientCredentials): Promise<string> {
+    const requestOptions: WebResource = new WebResource();
+    requestOptions.headers = {
+        ['User-Agent']: appendExtensionUserAgent()
+    };
+    requestOptions.url = url;
+    await signRequest(requestOptions, credentials);
+    // tslint:disable-next-line: await-promise
+    const response = await request(requestOptions).promise();
+    return <string>(response);
+}
+
+export async function sendRequest(httpReq: nRequest): Promise<IFunctionHostKeyResponse> {
+    return await <Thenable<IFunctionHostKeyResponse>>request(httpReq).promise();
 }
