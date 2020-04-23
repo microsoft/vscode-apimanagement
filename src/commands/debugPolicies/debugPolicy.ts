d77df79a3621a5092ba949dcebdb7beda28ed5cd
Rupeng Liu <rupliu@microsoft.com>
2020-04-23 14:05:23 -0700

    Change dogfood endpoint

--- src/commands/debugPolicies/debugPolicy.ts
+++ src/commands/debugPolicies/debugPolicy.ts
@@ -16,10 +16,9 @@ export async function debugApiPolicy(node?: ApiOperationTreeItem): Promise<void>
     }
 
     // tslint:disable-next-line: no-non-null-assertion
-    const gatewayAddress = getDebugGatewayAddressUrl(node!.root.serviceName);
+    const gatewayAddress = getDebugGatewayAddressDogfoodUrl(node!.root.serviceName);
     const managementUrl = `${node.root.environment.resourceManagerEndpointUrl}/subscriptions/${node.root.subscriptionId}/resourceGroups/${node.root.resourceGroupName}/providers/microsoft.apimanagement/service/${node.root.serviceName}`;
-
-    const mode = "development";
+    //const mode = "development";
 
     const debugConfig: vscode.DebugConfiguration = {
         type: "apim-policy",
@@ -31,22 +30,23 @@ export async function debugApiPolicy(node?: ApiOperationTreeItem): Promise<void>
         subscriptionId: node.root.subscriptionId
     };
 
-    const debugConfig2: vscode.DebugConfiguration = {
-        type: "apim-policy",
-        request: "launch",
-        name: "Attach to APIM",
-        stopOnEntry: true,
-        gatewayAddress: 'wss://proxy.apim.net/debug-0123456789abcdef',
-        managementAddress: 'https://management.apim.net/subscriptions/x/resourceGroups/x/providers/microsoft.apimanagement/service/x',
-        managementAuth: 'SharedAccessSignature integration&202004300042&Hh3jin6eJ22yc+MSAkaUl6+TgHwq8rRhnKt2UqQMugkiDsK5N3wJWWhjQscHiws0FID9ogk7sa0i7W/h2ydczA=='
-    };
+    // const debugConfig2: vscode.DebugConfiguration = {
+    //     type: "apim-policy",
+    //     request: "launch",
+    //     name: "Attach to APIM",
+    //     stopOnEntry: true,
+    //     gatewayAddress: 'wss://proxy.apim.net/debug-0123456789abcdef',
+    //     managementAddress: 'https://management.apim.net/subscriptions/x/resourceGroups/x/providers/microsoft.apimanagement/service/x',
+    //     managementAuth: 'SharedAccessSignature integration&202004300042&Hh3jin6eJ22yc+MSAkaUl6+TgHwq8rRhnKt2UqQMugkiDsK5N3wJWWhjQscHiws0FID9ogk7sa0i7W/h2ydczA=='
+    // };
 
     if (!vscode.debug.activeDebugSession) {
-        if (mode === "development") {
-            await vscode.debug.startDebugging(undefined, debugConfig2);
-        } else {
-            await vscode.debug.startDebugging(undefined, debugConfig);
-        }
+        await vscode.debug.startDebugging(undefined, debugConfig);
+        // if (mode === "development") {
+        //     await vscode.debug.startDebugging(undefined, debugConfig2);
+        // } else {
+        //     await vscode.debug.startDebugging(undefined, debugConfig);
+        // }
     }
     // tslint:disable-next-line: no-non-null-assertion
     await createOperationTestFile(node!, OperationRunMode.debug);
@@ -54,6 +54,10 @@ export async function debugApiPolicy(node?: ApiOperationTreeItem): Promise<void>
 
 export function getDebugGatewayAddressUrl(serviceName: string): string {
     return `wss://${serviceName}.azure-api.net/debug-0123456789abcdef`;
+}
+
+export function getDebugGatewayAddressDogfoodUrl(serviceName: string): string {
+    return `wss://${serviceName}.preview.int-azure-api.net/debug-0123456789abcdef`;
 }
 
 export interface IAuthenticationToken {
