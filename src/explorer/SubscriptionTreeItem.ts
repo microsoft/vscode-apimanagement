abf3e0f7cbae7b07fffb98fb2013809fc32c560b
RupengLiu <rupliu@microsoft.com>
2021-04-22 14:14:59 -0700

    Create subscription (#202)
    * Create subscription
    
    * Delete subscriptions
    
    * test commit
    
    * Add Subscriptions

--- src/explorer/SubscriptionTreeItem.ts
+++ src/explorer/SubscriptionTreeItem.ts
@@ -4,18 +4,20 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { SubscriptionContract } from "@azure/arm-apimanagement/src/models";
-import { AzureTreeItem, ISubscriptionContext } from "vscode-azureextensionui";
+import { ProgressLocation, window } from "vscode";
+import { AzureTreeItem, DialogResponses, ISubscriptionContext, UserCancelledError } from "vscode-azureextensionui";
+import { localize } from "../localize";
 import { nonNullProp } from "../utils/nonNull";
 import { treeUtils } from "../utils/treeUtils";
-import { IServiceTreeRoot } from "./IServiceTreeRoot";
+import { ISubscriptionTreeRoot } from "./ISubscriptionTreeRoot";
 import { SubscriptionsTreeItem } from "./SubscriptionsTreeItem";
 
-export class SubscriptionTreeItem extends AzureTreeItem<IServiceTreeRoot> {
+export class SubscriptionTreeItem extends AzureTreeItem<ISubscriptionTreeRoot> {
     public static contextValue: string = 'azureApiManagementSubscriptionTreeItem';
     public contextValue: string = SubscriptionTreeItem.contextValue;
 
     private _label: string;
-    private _root: IServiceTreeRoot;
+    private _root: ISubscriptionTreeRoot;
 
     constructor(
         parent: SubscriptionsTreeItem,
@@ -34,21 +36,37 @@ export class SubscriptionTreeItem extends AzureTreeItem<IServiceTreeRoot> {
         return this._label;
     }
 
-    public get root(): IServiceTreeRoot {
+    public get root(): ISubscriptionTreeRoot {
         return this._root;
     }
 
     public get iconPath(): { light: string, dark: string } {
-        return treeUtils.getThemedIconPath('gateway');
+        return treeUtils.getThemedIconPath('subscription');
     }
 
     public hasMoreChildrenImpl(): boolean {
         return false;
     }
 
-    private createRoot(subRoot: ISubscriptionContext): IServiceTreeRoot {
-        return Object.assign({}, <IServiceTreeRoot>subRoot, {
-            subscriptionName: nonNullProp(this.subscriptionContract, 'name')
+    public async deleteTreeItemImpl() : Promise<void> {
+        const message: string = localize("confirmDeleteSubscription", `Are you sure you want to delete subscription '${this.root.subscriptionSid}'?`);
+        const result = await window.showWarningMessage(message, { modal: true }, DialogResponses.deleteResponse, DialogResponses.cancel);
+        if (result === DialogResponses.deleteResponse) {
+            const deletingMessage: string = localize("", `Deleting subscription "${this.root.subscriptionSid}"...`);
+            await window.withProgress({ location: ProgressLocation.Notification, title: deletingMessage }, async () => {
+                await this.root.client.subscription.deleteMethod(this.root.resourceGroupName, this.root.serviceName, this.root.subscriptionSid, '*');
+            });
+            window.showInformationMessage(localize("deletedSubscription", `Successfully deleted Subscription "${this.root.subscriptionSid}".`));
+
+        } else {
+            throw new UserCancelledError();
+        }
+    }
+
+    private createRoot(subRoot: ISubscriptionContext): ISubscriptionTreeRoot {
+        return Object.assign({}, <ISubscriptionTreeRoot>subRoot, {
+            // tslint:disable-next-line: no-non-null-assertion
+            subscriptionSid: this.subscriptionContract.name!
         });
     }
 }
