68d45432fbf2e4391907563064df2c332ca2f474
ross-p-smith <rosmith@microsoft.com>
2023-10-17 16:52:39 +0100

    Added MIT License

--- src/utils/ApiOpsTooling.ts
+++ src/utils/ApiOpsTooling.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import { ServiceClient } from '@azure/ms-rest-js';
 import * as path from 'path';
 import * as vscode from 'vscode';
@@ -115,24 +119,3 @@ interface IAsset {
     updated_at:           Date;
     browser_download_url: string;
 }
-
-// interface IAuthor {
-//     login:               string;
-//     id:                  number;
-//     node_id:             string;
-//     avatar_url:          string;
-//     gravatar_id:         string;
-//     url:                 string;
-//     html_url:            string;
-//     followers_url:       string;
-//     following_url:       string;
-//     gists_url:           string;
-//     starred_url:         string;
-//     subscriptions_url:   string;
-//     organizations_url:   string;
-//     repos_url:           string;
-//     events_url:          string;
-//     received_events_url: string;
-//     type:                string;
-//     site_admin:          boolean;
-// }
--- src/utils/azUtils.ts
+++ src/utils/azUtils.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import * as vscode from 'vscode';
 import { DialogResponses, IAzExtOutputChannel } from "../../extension.bundle";
 import { localize } from "../localize";
