066de56ead29afd281d85d5c7e0c6e8c08c3f2eb
blackchoey <blackchoey@gmail.com>
2025-03-13 22:24:33 +0800

    feat: language model tool poc

---
+++ src/tools/availablePoliciesTool.ts
@@ -0,0 +1,40 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class AvailablePoliciesTool implements vscode.LanguageModelTool<{}> {
+    async prepareInvocation(
+        _options: vscode.LanguageModelToolInvocationPrepareOptions<{}>,
+        _token: vscode.CancellationToken
+    ) {
+
+        return {
+            invocationMessage: 'Getting available APIM policies'
+        };
+    }
+
+    async invoke(
+        _options: vscode.LanguageModelToolInvocationOptions<{}>,
+        _token: vscode.CancellationToken
+    ): Promise<vscode.LanguageModelToolResult> {
+        try {
+            const policiesPath = path.join(__dirname, '..', '..', 'resources', 'knowledgeBase', 'policies.json');
+            const policiesContent = fs.readFileSync(policiesPath, 'utf8');
+            const policies = JSON.parse(policiesContent);
+
+            const policyList = Object.entries(policies)
+                .map(([name, description]) => `${name}: ${description}`)
+                .join('\n\n');
+
+            console.log(policyList)
+
+            return new vscode.LanguageModelToolResult([
+                new vscode.LanguageModelTextPart('Here are all available APIM policies:\n\n' + policyList)
+            ]);
+        } catch (error) {
+            return new vscode.LanguageModelToolResult([
+                new vscode.LanguageModelTextPart(`Error retrieving policies: ${error.message}`)
+            ]);
+        }
+    }
+}
