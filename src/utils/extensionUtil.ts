4fcac99bf3c97d4cee907d4120a0f531cf0efb75
Chaoyi Yuan <chyuan@microsoft.com>
2025-03-17 15:24:00 +0800

    build: remove unused code

--- src/utils/extensionUtil.ts
+++ src/utils/extensionUtil.ts
@@ -5,29 +5,6 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import { DialogResponses, IActionContext } from '@microsoft/vscode-azext-utils';
-import { localize } from '../localize';
-import { openUrl } from '../utils/openUrl';
-
-// tslint:disable-next-line:export-name
-export function checkCsharpExtensionInstalled(actionContext: IActionContext): void {
-    const csharpExtension = vscode.extensions.getExtension("ms-dotnettools.csharp") || vscode.extensions.getExtension("ms-vscode.csharp");
-    if (!csharpExtension) {
-        const message: string = localize('csharpExtensionNotInstalled', 'You must have the VSCode CSharp extension installed to improve policy authoring experience.');
-
-        if (!actionContext.errorHandling.suppressDisplay) {
-            // don't wait
-            vscode.window.showErrorMessage(message, DialogResponses.learnMore).then(async (result) => {
-                if (result === DialogResponses.learnMore) {
-                    await openUrl('https://marketplace.visualstudio.com/items?itemName=ms-vscode.csharp');
-                }
-            });
-            actionContext.errorHandling.suppressDisplay = true;
-        }
-
-        throw new Error(message);
-    }
-}
 
 export async function showReleaseNotes(context: vscode.ExtensionContext): Promise<void> {
     const packageJson = JSON.parse(fs.readFileSync(path.join(context.extensionPath, 'package.json'), 'utf8'));
