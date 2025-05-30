008643b701e16f3418bdb1a014ebb46ad5e56334
Chaoyi Yuan <blackchoey@gmail.com>
2025-05-30 15:58:00 +0800

    build: fix vulnerabilities (#435)

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,22 +3,7 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// tslint:disable:no-unsafe-any
-
-import * as cp from 'child_process';
-import * as gulp from 'gulp';
-import * as path from 'path';
-import { gulp_installAzureAccount, gulp_webpack } from '@microsoft/vscode-azext-dev';
-import { gulp_installRestClient } from './gulp/gulp_installRestClient';
-
-const env = process.env;
-
-function test(): cp.ChildProcess {
-    env.DEBUGTELEMETRY = '1';
-    env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
-    return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
-}
+import { gulp_webpack } from '@microsoft/vscode-azext-dev';
 
 exports['webpack-dev'] = () => gulp_webpack('development');
 exports['webpack-prod'] = () => gulp_webpack('production');
-exports.test = gulp.series(gulp_installAzureAccount, gulp_installRestClient, test);
